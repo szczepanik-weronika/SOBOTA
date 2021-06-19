96a390e58207820d42efae9ded8d60086aba679e
Masive <jozollo@gmail.com>
2021-06-19 09:57:53 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main (String args[]){
+
+        int x =3;
+        int y;
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x);
+
+        y= x++;
+        System.out.println(y);
+        y = ++x;
+        System.out.println(y);
+        System.out.println(++y);
+    }
+}
+
